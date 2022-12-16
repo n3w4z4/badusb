3fa70e47a46888f4a1abf5f63e46f3f6aad5a4ca
N3w4z4 <60519669+n3w4z4@users.noreply.github.com>
2022-12-16 15:17:53 +0100

    Update example.ps1

--- payloads/powershell/example.ps1
+++ payloads/powershell/example.ps1
@@ -1,3 +1,5 @@
 [console]::WindowHeight=1
 [console]::WindowWidth=1
+[System.Windows.MessageBox]::Show('Would  you like to play a game?','Game input','YesNoCancel','Error')
 calc.exe
+[System.Windows.MessageBox]::Show('Would  you like to play a game?','Game input','YesNoCancel','Error')
